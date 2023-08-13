a075b235bc9e974bdf86223a03c5a2c9386eff79
chuhha <57899753+chuhha@users.noreply.github.com>
2023-08-13 13:53:15 +0300

    Add files via upload

---
+++ bar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Иконка загрузки в консоли
+function show_progress {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    while ps a | awk '{print $1}' | grep -q "$pid"; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+# Тест работоспособности
+sleep 100   &
+show_progress $!
